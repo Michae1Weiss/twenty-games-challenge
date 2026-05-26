179dfb63454ba013a75270d389860382c9a77eb9
Michae1Weiss <misha19989@icloud.com>
2026-05-26 21:39:34 +0200

    wip: add new game textures

--- game_01_breakout/src/game/assets.rs
+++ game_01_breakout/src/game/assets.rs
@@ -3,11 +3,11 @@ use bevy_asset_loader::asset_collection::AssetCollection;
 
 #[derive(Resource, AssetCollection)]
 pub struct TextureAssets {
-    #[asset(path = "textures/paddle-oversimplified.png")]
+    #[asset(path = "textures/paddle-wood.png")]
     pub paddle: Handle<Image>,
-    #[asset(path = "textures/brick-oversimplified.png")]
+    #[asset(path = "textures/brick-wood.png")]
     pub brick: Handle<Image>,
-    #[asset(path = "textures/background.png")]
+    #[asset(path = "textures/background-felt.png")]
     pub background: Handle<Image>,
     #[asset(path = "textures/danger-zone.png")]
     pub danger_zone: Handle<Image>,
